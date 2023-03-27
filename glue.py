815c7894b7abecfd54dca7f9cc791ab1e5222a78
lebwa <lebwa82@gmail.com>
2023-03-28 01:04:24 +0300

    5

--- glue.py
+++ glue.py
@@ -4,7 +4,8 @@ es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 import json
 import time
 import re
-
+glue_text = open('glue_text.txt', 'w')
+start = time.time()
 
 N_results = 5
 def elastic_more_like_this(sentence):
@@ -21,7 +22,9 @@ def elastic_more_like_this(sentence):
     count = 0
     for element in a['hits']['hits']:
         el = element['_source']['text'].strip()
-        res += f' ++++++++++++++++++++++++++++++++ {el}'
+        #отделить знаки препинания от текста
+        re_el = re.sub(r'[]!"$%&\'()*+,./:;=#@?[\\^_`{|}~-]+', r' \g<0> ', el).strip()
+        res += f' ++++++++++++++++++++++++++++++++ {re_el}'
         count += 1
         if count > 5:
             break
@@ -30,11 +33,16 @@ def elastic_more_like_this(sentence):
 
 file = open("spanner.test")
 conll = json.load(file)
+count_sentence = 0
 for sentence in conll:
+    count_sentence += 1
+    if count_sentence %100 == 0:
+        print(f'count_sentence = {count_sentence} time = {time.time() - start}')
     span_posLabel = sentence['span_posLabel']
     context = sentence['context']
     res1  = context + elastic_more_like_this(context)
     #отделить знаки препинания от текста
-    res2 = re.sub(r'[]!"$%&\'()*+,./:;=#@?[\\^_`{|}~-]+', r' \g<0> ', res1).strip()
-    print(f'context = {context}\n res = {res2}\n')
-    input()
+    #res2 = re.sub(r'[]!"$%&\'()*+,./:;=#@?[\\^_`{|}~-]+', r' \g<0> ', res1).strip()
+    #print(f'context = {context}\n res = {res2}\n')
+    glue_text.write(f'{res1}\n')
+    #input()
